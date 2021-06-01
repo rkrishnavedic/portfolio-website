9ac53c6b2116f9cea251fc89e72db5e6d151c942
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2021-06-01 19:10:56 +0530

    style: responsive

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -29,28 +29,29 @@ const data = [
 export default function Projects(){
     return(
 
-            <div className="projects-container">
-                <h2 className="heading">
-                    PROJECTS
-                </h2>
-                <div className="data">
-                {data.map((_project)=>{
-                    return(
-                        <div className="project">
-                            <div className="project-left">
-                                <h2>{_project.title}</h2>
-                                <p>{_project.desc}</p>
-                                <div className='button-set'>
-                                    <button>See live</button>
-                                    <button>code</button>
-                                </div>
-                            </div>
-                            <div className="project-right">
+            <div className="odd-container">
+                
+                <div className="card-space">
+                    <div className="void-card">
+                        <h4>Projects</h4>
+                    </div>
+                    {data.map((_project)=>{
+                        return(
+                            <div className="odd-card">
                                 <img src="https://sm.pcmag.com/pcmag_in/guide/t/the-best-p/the-best-project-management-software-for-2021_ehu1.jpg" alt='pro'/>
+                                
+                                <div className="card-detail">
+                                    <h2>{_project.title}</h2>
+                                    <p>{_project.desc}</p>
+                                    <div className='button-set'>
+                                        <button>See live</button>
+                                        <button>code</button>
+                                    </div>
+                                </div>
+                                
                             </div>
-                        </div>
-                    )
-                })}
+                        )
+                    })}
                 </div>
                 
             </div>
