7d01c64784b7788af80bfef50a701986ecbe116d
Rahul Krishna <57654450+rkrishnavedic@users.noreply.github.com>
2021-06-03 20:37:40 +0530

    docs

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,13 +16,7 @@ const data = [
         live: '',
         background: ''
     },
-    {
-        title: 'Project 1',
-        desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        code: 'https://www.google.com',
-        live: '',
-        background: ''
-    },
+    
     
 ]
 
